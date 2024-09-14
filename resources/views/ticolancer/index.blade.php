f7956c67dc0c02ce808702b1c2bfaaf77b2439a8
Jonathan <143845545+corralesjonathan@users.noreply.github.com>
2024-09-13 23:11:12 -0600

    create images folder

--- resources/views/ticolancer/index.blade.php
+++ resources/views/ticolancer/index.blade.php
@@ -145,64 +145,64 @@
     {{-- Services end --}}
 
     {{-- Popular services --}}
-        <div class="flex flex-col justify-center items-center bg-bg py-[40px] gap-[40px] m-auto max-sm:w-[90vw]">
-            <h2 class="text-blue text-[36px] max-sm:text-[24px] font-light">Servicios <span class="text-green font-secondary">populares</span></h2>
-            <div class="grid grid-cols-6 gap-[20px] max-sm:grid-cols-2">
-                {{-- Desarrollo web --}}
-                <a href="">
+    <div class="flex flex-col justify-center items-center bg-bg py-[40px] gap-[40px] m-auto max-sm:w-[90vw]">
+        <h2 class="text-blue text-[36px] max-sm:text-[24px] font-light">Servicios <span class="text-green font-secondary">populares</span></h2>
+        <div class="grid grid-cols-6 gap-[20px] max-sm:grid-cols-2">
+            {{-- Desarrollo web --}}
+            <a href="">
                     <div class="flex flex-col justify-center items-start bg-[#00732E] gap-[20px] max-sm:gap-[10px] p-[10px] rounded-[16px] hover:bg-opacity-85 transition-all duration-500 ease-out">
                         <h5 class="h-[60px] text-white font-semibold text-[20px] max-sm:text-[16px] p-[5px]">Desarrollo Web</h5>
                         <img class="rounded-[16px]" src="{{ asset('storage/images/desarrollo_web.jpg') }}" alt="">
                     </div>
-                </a>
-                {{-- Desarrollo web end --}}
+            </a>
+            {{-- Desarrollo web end --}}
 
-                {{-- Diseño de logo--}}
-                <a href="">
+            {{-- Diseño de logo--}}
+            <a href="">
                     <div class="flex flex-col justify-center items-start bg-[#F97B4D] gap-[20px] max-sm:gap-[10px] p-[10px] rounded-[16px] hover:bg-opacity-85 transition-all duration-500 ease-out">
                         <h5 class="h-[60px] text-white font-semibold text-[20px] max-sm:text-[16px] p-[5px]">Diseño de logo</h5>
                         <img class="rounded-[16px]" src="{{ asset('storage/images/diseno_logo.jpg') }}" alt="">
                     </div>
-                </a>
-                {{-- Diseño de logo end --}}
+            </a>
+            {{-- Diseño de logo end --}}
 
-                {{-- Marketing --}}
-                <a href="">
+            {{-- Marketing --}}
+            <a href="">
                     <div class="flex flex-col justify-center items-start bg-[#687200] gap-[20px] max-sm:gap-[10px] p-[10px] rounded-[16px] hover:bg-opacity-85 transition-all duration-500 ease-out">
                         <h5 class="h-[60px] text-white font-semibold text-[20px] max-sm:text-[16px] p-[5px]">Marketing</h5>
                         <img class="rounded-[16px]" src="{{ asset('storage/images/marketing.jpg') }}" alt="">
                     </div>
-                </a>
-                {{-- Marketing end --}}
+            </a>
+            {{-- Marketing end --}}
 
-                {{-- arquitectura y diseño --}}
-                <a href="">
+            {{-- arquitectura y diseño --}}
+            <a href="">
                     <div class="flex flex-col justify-center items-start bg-[#4D1727] gap-[20px] max-sm:gap-[10px] p-[10px] rounded-[16px] hover:bg-opacity-85 transition-all duration-500 ease-out">
                         <h5 class="w-[180px] h-[60px]  text-white font-semibold text-[20px] max-sm:text-[16px] p-[5px]">Arquitectura y Diseño Interior</h5>
                         <img class="rounded-[16px]" src="{{ asset('storage/images/arquitectura.jpg') }}" alt="">
                     </div>
-                </a>
-                {{-- arquitectura y diseño end --}}
+            </a>
+            {{-- arquitectura y diseño end --}}
 
-                {{-- desarrollo de software --}}
-                <a href="">
+            {{-- desarrollo de software --}}
+            <a href="">
                     <div class="flex flex-col justify-center items-start bg-[#254200] gap-[20px] max-sm:gap-[10px] p-[10px] rounded-[16px] hover:bg-opacity-85 transition-all duration-500 ease-out">
                         <h5 class="w-[180px] h-[60px]  text-white font-semibold text-[20px] max-sm:text-[16px] p-[5px]">Desarrollo de Software</h5>
                         <img class="rounded-[16px]" src="{{ asset('storage/images/dev.jpg') }}" alt="">
                     </div>
-                </a>
-                {{-- desarrollo de software end --}}
+            </a>
+            {{-- desarrollo de software end --}}
 
-                {{-- video --}}
-                <a href="">
+            {{-- video --}}
+            <a href="">
                     <div class="flex flex-col justify-center items-start bg-[#C66783] gap-[20px] max-sm:gap-[10px] p-[10px] rounded-[16px] hover:bg-opacity-85 transition-all duration-500 ease-out">
                         <h5 class="w-[180px] h-[60px] text-white font-semibold text-[20px] max-sm:text-[16px] p-[5px]">Edición de Video</h5>
                         <img class="rounded-[16px]" src="{{ asset('storage/images/video.jpg') }}" alt="">
                     </div>
-                </a>
-                {{-- video end --}}
-            </div>
+            </a>
+            {{-- video end --}}
         </div>
+    </div>
     {{-- Popular services end --}}
 
     {{-- Quienes somos --}}
