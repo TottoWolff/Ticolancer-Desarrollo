1e781022b2a9e5a874416fc6eaebecce8513e0c0
TottoWolff <wolff5eru9@gmail.com>
2024-10-09 15:33:40 -0600

    URL Images replaced by PNG, JPG and JPEG on gigCreation

--- app/Http/Controllers/GigsController.php
+++ app/Http/Controllers/GigsController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\GigsTicolancer;
-use App\Models\ProvincesTicolancer;
-use App\Models\CitiesTicolancer;
 use Illuminate\Support\Facades\Auth;
 
 class GigsController extends Controller
@@ -13,21 +11,21 @@ class GigsController extends Controller
     public function index()
     {
         $sessionActive = Auth::guard('buyers')->check();
-    
+
         if (!$sessionActive) {
             return redirect()->route('login');
         } else {
             $user = Auth::guard('buyers')->user();
-    
+
             if ($user) {
                 $username = $user->username;
                 return view('sellers.sellerGigs', ['username' => $username], compact('username'));
             } else {
-                return redirect()->route('login'); // En caso de que no haya usuario autenticado
+                return redirect()->route('login');
             }
         }
     }
-    
+
 
     /**
      * Show the form for creating a new resource.
@@ -51,35 +49,42 @@ class GigsController extends Controller
     {
         if (!Auth::guard('buyers')->check()) {
             return redirect()->route('login');
-        }else{
-            
+        } else {
             $user = Auth::guard('buyers')->user();
 
-            
             $request->validate([
                 'gig_name' => 'required',
-                'gig_image' => 'required|url',
+                'gig_image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
                 'gig_description' => 'required',
                 'gig_email' => 'required|email',
                 'gig_phone_number' => 'required',
                 'province_id' => 'required|exists:provinces_ticolancers,id',
                 'city_id' => 'required|exists:cities_ticolancers,id',
             ]);
-            
+
             $gig = new GigsTicolancer;
             $gig->gig_name = $request->gig_name;
-            $gig->gig_image = $request->gig_image;
             $gig->gig_description = $request->gig_description;
             $gig->gig_email = $request->gig_email;
             $gig->gig_phone_number = $request->gig_phone_number;
             $gig->provinces_ticolancers_id = $request->province_id;
             $gig->cities_ticolancers_id = $request->city_id;
             $gig->sellers_users_ticolancers_id = $user->id;
+
+            if ($request->hasFile('gig_image')) {
+                $file = $request->file('gig_image');
+                $fileType = $file->getClientOriginalExtension();
+                $filename = $gig->gig_name . '.' . $fileType;
+                $file->move(public_path('images/gigs'), $filename);
+                $gig->gig_image = $filename;
+            }
+
             $gig->save();
-            
+
             return redirect()->back()->with('warning', 'Gig creado exitosamente');
         }
     }
+
 
     /**
      * Show the form for editing the specified resource.
