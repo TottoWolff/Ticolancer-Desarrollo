ca3ae9e363e9ab9c1f0936f07320b3c8156263c5
Alejandro Robles Bolaños <alejandrorb0803@gmail.com>
2024-10-13 00:23:53 -0600

    add dashboard mdal

--- resources/views/ticolancer/index.blade.php
+++ resources/views/ticolancer/index.blade.php
@@ -207,124 +207,174 @@
 
 {{-- Servicios --}}
 
-<div class="flex flex-col justify-center items-center bg-bg py-[40px] gap-[60px] m-auto max-sm:w-[90vw] mx-16">
-    <h2 class="text-blue text-[36px] max-sm:text-[28px] font-light">Todos los <span class="text-green font-secondary">Servicios</span></h2>
 
-    <div class="grid grid-cols-6 gap-[30px] max-sm:grid-cols-2">
-        @if ($gigs && count($gigs) > 0)
-        @foreach ($gigs as $gig)
-        <div class="border border-gray-300 rounded-lg p-4 shadow-md">
-            @if (Auth::guard('buyers')->check())
-            <a href="{{ route('sellerGig', ['id' => $gig->id, 'username' => $username]) }}" class="block w-full cursor-pointer">
-                @else
-                <a href="{{ route('login') }}" class="block w-full cursor-pointer">
-                    @endif
-                    <div class="flex flex-col items-center">
-                        <!-- Imagen del gig -->
-                        <img class="w-full h-40 object-cover rounded-md cursor-pointer mb-4" src="{{ asset('images/gigs/' . $gig->gig_image) }}" alt="{{ $gig->gig_name }}">
+    <div class="flex flex-col justify-center items-center bg-bg py-[40px] gap-[60px] m-auto max-sm:w-[90vw] mx-16">
+        <h2 class="text-blue text-[36px] max-sm:text-[28px] font-light">Todos los <span class="text-green font-secondary">Servicios</span></h2>
 
-                        <!-- Nombre del gig -->
-                        <span class="text-[16px] max-sm:text-[14px] font-light text-black">{{ $gig->gig_name }}</span>
+        <div class="grid grid-cols-6 gap-[30px] max-sm:grid-cols-2">
+            @if ($gigs && count($gigs) > 0)
+            @foreach ($gigs as $gig)
+            <div class="border border-gray-300 rounded-lg p-4 shadow-md">
+                @if (Auth::guard('buyers')->check())
+                <a href="{{ route('sellerGig', ['id' => $gig->id, 'username' => $username]) }}" class="block w-full cursor-pointer service-link" data-authenticated="true">
+                    @else
+                    <a href="#" class="block w-full cursor-pointer service-link" data-authenticated="false">
+                        @endif
+                        <div class="flex flex-col items-center">
+                            <!-- Imagen del gig -->
+                            <img class="w-full h-40 object-cover rounded-md cursor-pointer mb-4" src="{{ asset('images/gigs/' . $gig->gig_image) }}" alt="{{ $gig->gig_name }}">
 
-                        <!-- Calificación y cantidad de reseñas -->
-                        <div class="flex gap-2 mt-2 items-center">
-                            <img class="w-5 h-5" src="{{ asset('images/profile/star.png') }}" alt="Calificación">
-                            <span class="text-primary font-semibold text-[15px] max-sm:text-[12px]">
-                                {{ $gig->reviews->isNotEmpty() ? number_format(optional($gig->reviews->first())->average_rating, 1) : 'Sin calificaciones' }} / 5
-                            </span>
-                            <span class="text-gray-400 text-[15px] max-sm:text-[10px]">(221)</span>
+                            <!-- Nombre del gig -->
+                            <span class="text-[16px] max-sm:text-[14px] font-light text-black">{{ $gig->gig_name }}</span>
+
+                            <!-- Calificación y cantidad de reseñas -->
+                            <div class="flex gap-2 mt-2 items-center">
+                                <img class="w-5 h-5" src="{{ asset('images/profile/star.png') }}" alt="Calificación">
+                                <span class="text-primary font-semibold text-[15px] max-sm:text-[12px]">
+                                    {{ $gig->reviews->isNotEmpty() ? number_format(optional($gig->reviews->first())->average_rating, 1) : 'Sin calificaciones' }} / 5
+                                </span>
+                                <span class="text-gray-400 text-[15px] max-sm:text-[10px]">(221)</span>
+                            </div>
+
+                            <!-- Precio del gig -->
+                            <div class="text-primary font-semibold text-[16px] max-sm:text-[12px] mt-2">Desde ₡{{ $gig->gig_price }}</div>
                         </div>
-
-                        <!-- Precio del gig -->
-                        <div class="text-primary font-semibold text-[16px] max-sm:text-[12px] mt-2">Desde ₡{{ $gig->gig_price }}</div>
-                    </div>
+                    </a>
+            </div>
+            @endforeach
+            @else
+            <div class="flex flex-col items-center justify-center place-content-center text-center p-10 bg-blue-50 border border-blue-200 rounded-lg shadow-md mt-10">
+                <h2 class="text-2xl font-bold text-blue-600">¡Lo sentimos!</h2>
+                <p class="text-lg text-blue-500 mt-2">Actualmente no hay servicios disponibles.</p>
+                <p class="text-gray-600 mt-2">Vuelve más tarde o explora otros perfiles.</p>
+                <a href="{{ url('/sellers/{username}/dashboardgigs') }}" class="mt-5 inline-block bg-blue-600 text-white py-2 px-4 rounded-full shadow-lg hover:bg-blue-700 transition duration-300 ease-in-out">
+                    Explorar más servicios
                 </a>
+            </div>
+            @endif
         </div>
-        @endforeach
-        @else
-        <div class="flex flex-col items-center justify-center place-content-center text-center p-10 bg-blue-50 border border-blue-200 rounded-lg shadow-md mt-10">
-            <h2 class="text-2xl font-bold text-blue-600">¡Lo sentimos!</h2>
-            <p class="text-lg text-blue-500 mt-2">Actualmente no hay servicios disponibles.</p>
-            <p class="text-gray-600 mt-2">Vuelve más tarde o explora otros perfiles.</p>
-            <a href="{{ url('/sellers/{username}/dashboardgigs') }}" class="mt-5 inline-block bg-blue-600 text-white py-2 px-4 rounded-full shadow-lg hover:bg-blue-700 transition duration-300 ease-in-out">
-                Explorar más servicios
-            </a>
-        </div>
-        @endif
     </div>
-</div>
 
 
-{{-- Servicios end --}}
-
-{{-- Quienes somos --}}
-<div class="flex w-full bg-blue py-[40px]">
-    <div class="flex flex-col justify-center items-center m-auto w-[90vw] gap-[40px]">
-        <h2 class="text-white w-fit text-[36px] max-sm:text-[28px] font-light">¿Quiénes <span class="text-green font-secondary">somos?</span></h2>
-        <div class="grid grid-cols-3 max-sm:grid-cols-1 max-sm:gap-[40px] w-full">
-            <div class="flex flex-col justify-between items-center h-[200px] gap-[20px]">
-                <img class="w-[80px] max-sm:w-[60px]" src="{{ asset('icons/grow.svg') }}" alt="Grow">
-                <div class="flex flex-col items-center gap-[10px]">
-                    <h3 class="text-[24px] font-semibold text-white">Crece como freelancer</h3>
-                    <p class="text-[16px] text-center font-light text-white">Ticolancer está diseñado para conectar a
-                        profesionales independientes en Costa Rica
-                        con quienes buscan sus servicios.</p>
-                </div>
-            </div>
-
-            <div class="flex flex-col justify-between items-center h-[200px] gap-[20px]">
-                <img class="w-[80px] max-sm:w-[60px]" src="{{ asset('icons/rocket.svg') }}" alt="Rocket">
-                <div class="flex flex-col items-center gap-[10px]">
-                    <h3 class="text-[24px] font-semibold text-white">Impulsa tu talento</h3>
-                    <p class="text-[16px] text-center font-light text-white">En Ticolancer, brindamos una plataforma dedicada
-                        a freelancers costarricenses, donde puedes ofrecer
-                        tus servicios a clientes locales y globales.</p>
-                </div>
-            </div>
-
-            <div class="flex flex-col justify-between items-center h-[200px] gap-[20px]">
-                <img class="w-[80px] max-sm:w-[60px]" src="{{ asset('icons/network.svg') }}" alt="Network">
-                <div class="flex flex-col items-center gap-[10px]">
-                    <h3 class="text-[24px] font-semibold text-white">Conecta con oportunidades</h3>
-                    <p class="text-[16px] text-center font-light text-white">Ticolancer es la plataforma ideal para freelancers
-                        ticos que buscan ampliar su red de clientes
-                        y aumentar su visibilidad.</p>
-                </div>
+    <!-- Modal -->
+    <div id="authModal" class="hidden fixed inset-0 bg-blue bg-opacity-50 backdrop-blur-sm flex items-center justify-center z-50">
+        <div class="bg-white rounded-lg p-8 max-w-sm w-full shadow-lg text-center">
+            <h2 class="text-xl font-semibold mb-4 text-gray-800">Acción no permitida</h2>
+            <p class="text-gray-600 mb-6">Para ver más detalles del servicio debes estar logueado   .</p>
+            <div class="flex justify-center gap-5">
+                <button id="closeModal" class="button px-[20px] py-[10px] rounded-[10px] text-blue font-semibold border-solid border-[1px] border-blue hover:translate-y-[-5px] transition-all duration-500 ease-out hover:bg-blue hover:text-white ">
+                    Cancelar
+                </button>
+                <a href="{{ route('login') }}" class="button px-[20px] py-[10px] rounded-[10px] text-blue font-semibold border-solid border-[1px] border-blue hover:translate-y-[-5px] transition-all duration-500 ease-out hover:bg-blue hover:text-white ">
+                    Iniciar Sesión
+                </a>
             </div>
         </div>
-        <a class="px-[20px] py-[10px] text-blue bg-white rounded-[10px] font-semibold hover:text-green hover:translate-y-[-5px] transition-all duration-500 ease-out " href="{{ route('nosotros') }}">Leer más</a>
     </div>
-</div>
-{{-- Quienes somos end --}}
 
-{{-- Seras el proximo --}}
-<div class="w-[90vw] m-auto grid grid-cols-2 max-sm:grid-cols-1 gap-[40px] py-[40px]">
-    <div class="flex justify-center items-start max-sm:items-center flex-col gap-[20px] ">
-        <h2 class="text-blue text-[36px] max-sm:text-[28px] font-light">¿Serás el <span class="text-green font-secondary">próximo?</span></h2>
-        <p class="text-[18px] max-sm:text-center font-light text-black">Tu puedes ser el próximo freelancer tico en nuestra web!</p>
-        <a class="px-[20px] py-[10px] rounded-[10px] text-blue font-semibold border-solid border-[1px] border-blue hover:translate-y-[-5px] transition-all duration-500 ease-out hover:bg-blue hover:text-white " href="{{ route('signup') }}">Registrarse</a>
-    </div>
-    <div class="flex justify-center items-start">
-        <img src="https://i.postimg.cc/pX0jkmrF/illustration.png" alt="">
-    </div>
-</div>
-{{-- Seras el proximo end --}}
 
-{{-- Enviar email --}}
-<div class="flex w-full bg-blue py-[40px]">
-    <div class="flex flex-col justify-center items-center m-auto w-[90vw] gap-[40px]">
-        <img src="https://i.postimg.cc/L5xn179L/illustration2.png" alt="">
-        <div class="flex flex-col w-full gap-[20px] max-sm:items-center">
-            <h2 class="text-white w-fit text-[36px] max-sm:text-[28px] font-light">¿Deseas <span class="text-green font-secondary">contactarnos?</span></h2>
-            <h3 class="text-white font-light text-[22px]">¡Únete a la familia Ticolancer!</h3>
-            <form class="flex flex-row" action="{{ route('subscription.store') }}" method="POST">
-                @csrf
-                <input type="text" name="email" placeholder="Email" class="rounded-l-[10px] w-[40%] max-sm:w-[100%] p-[10px] text-[16px] text-white bg-transparent border-[1px] border-white font-light outline-none">
-                <input type="submit" class="rounded-r-[10px] p-[10px] cursor-pointer text-[16px] text-primary font-semibold bg-white outline-none">
-            </form>
+    {{-- Servicios end --}}
+
+    {{-- Quienes somos --}}
+    <div class="flex w-full bg-blue py-[40px]">
+        <div class="flex flex-col justify-center items-center m-auto w-[90vw] gap-[40px]">
+            <h2 class="text-white w-fit text-[36px] max-sm:text-[28px] font-light">¿Quiénes <span class="text-green font-secondary">somos?</span></h2>
+            <div class="grid grid-cols-3 max-sm:grid-cols-1 max-sm:gap-[40px] w-full">
+                <div class="flex flex-col justify-between items-center h-[200px] gap-[20px]">
+                    <img class="w-[80px] max-sm:w-[60px]" src="{{ asset('icons/grow.svg') }}" alt="Grow">
+                    <div class="flex flex-col items-center gap-[10px]">
+                        <h3 class="text-[24px] font-semibold text-white">Crece como freelancer</h3>
+                        <p class="text-[16px] text-center font-light text-white">Ticolancer está diseñado para conectar a
+                            profesionales independientes en Costa Rica
+                            con quienes buscan sus servicios.</p>
+                    </div>
+                </div>
+
+                <div class="flex flex-col justify-between items-center h-[200px] gap-[20px]">
+                    <img class="w-[80px] max-sm:w-[60px]" src="{{ asset('icons/rocket.svg') }}" alt="Rocket">
+                    <div class="flex flex-col items-center gap-[10px]">
+                        <h3 class="text-[24px] font-semibold text-white">Impulsa tu talento</h3>
+                        <p class="text-[16px] text-center font-light text-white">En Ticolancer, brindamos una plataforma dedicada
+                            a freelancers costarricenses, donde puedes ofrecer
+                            tus servicios a clientes locales y globales.</p>
+                    </div>
+                </div>
+
+                <div class="flex flex-col justify-between items-center h-[200px] gap-[20px]">
+                    <img class="w-[80px] max-sm:w-[60px]" src="{{ asset('icons/network.svg') }}" alt="Network">
+                    <div class="flex flex-col items-center gap-[10px]">
+                        <h3 class="text-[24px] font-semibold text-white">Conecta con oportunidades</h3>
+                        <p class="text-[16px] text-center font-light text-white">Ticolancer es la plataforma ideal para freelancers
+                            ticos que buscan ampliar su red de clientes
+                            y aumentar su visibilidad.</p>
+                    </div>
+                </div>
+            </div>
+            <a class="px-[20px] py-[10px] text-blue bg-white rounded-[10px] font-semibold hover:text-green hover:translate-y-[-5px] transition-all duration-500 ease-out " href="{{ route('nosotros') }}">Leer más</a>
         </div>
     </div>
-</div>
-{{-- Enviar email end --}}
+    {{-- Quienes somos end --}}
 
-@endsection
+    {{-- Seras el proximo --}}
+    <div class="w-[90vw] m-auto grid grid-cols-2 max-sm:grid-cols-1 gap-[40px] py-[40px]">
+        <div class="flex justify-center items-start max-sm:items-center flex-col gap-[20px] ">
+            <h2 class="text-blue text-[36px] max-sm:text-[28px] font-light">¿Serás el <span class="text-green font-secondary">próximo?</span></h2>
+            <p class="text-[18px] max-sm:text-center font-light text-black">Tu puedes ser el próximo freelancer tico en nuestra web!</p>
+            <a class="px-[20px] py-[10px] rounded-[10px] text-blue font-semibold border-solid border-[1px] border-blue hover:translate-y-[-5px] transition-all duration-500 ease-out hover:bg-blue hover:text-white " href="{{ route('signup') }}">Registrarse</a>
+        </div>
+        <div class="flex justify-center items-start">
+            <img src="https://i.postimg.cc/pX0jkmrF/illustration.png" alt="">
+        </div>
+    </div>
+    {{-- Seras el proximo end --}}
+
+    {{-- Enviar email --}}
+    <div class="flex w-full bg-blue py-[40px]">
+        <div class="flex flex-col justify-center items-center m-auto w-[90vw] gap-[40px]">
+            <img src="https://i.postimg.cc/L5xn179L/illustration2.png" alt="">
+            <div class="flex flex-col w-full gap-[20px] max-sm:items-center">
+                <h2 class="text-white w-fit text-[36px] max-sm:text-[28px] font-light">¿Deseas <span class="text-green font-secondary">contactarnos?</span></h2>
+                <h3 class="text-white font-light text-[22px]">¡Únete a la familia Ticolancer!</h3>
+                <form class="flex flex-row" action="{{ route('subscription.store') }}" method="POST">
+                    @csrf
+                    <input type="text" name="email" placeholder="Email" class="rounded-l-[10px] w-[40%] max-sm:w-[100%] p-[10px] text-[16px] text-white bg-transparent border-[1px] border-white font-light outline-none">
+                    <input type="submit" class="rounded-r-[10px] p-[10px] cursor-pointer text-[16px] text-primary font-semibold bg-white outline-none">
+                </form>
+            </div>
+        </div>
+    </div>
+    {{-- Enviar email end --}}
+
+    @endsection
+
+    <script>
+      document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.getElementById('authModal');
+    const closeModal = document.getElementById('closeModal');
+    const serviceLinks = document.querySelectorAll('.service-link');
+    const modalContent = modal.querySelector('.bg-white'); // Selecciona el contenido del modal
+
+    // Mostrar el modal si el usuario no está autenticado
+    serviceLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            if (link.dataset.authenticated === 'false') {
+                e.preventDefault();
+                modal.classList.remove('hidden');
+            }
+        });
+    });
+
+    // Cerrar el modal al hacer clic en el botón "Cancelar"
+    closeModal.addEventListener('click', function () {
+        modal.classList.add('hidden');
+    });
+
+    // Cerrar el modal al hacer clic fuera del contenido del modal
+    modal.addEventListener('click', function (e) {
+        if (!modalContent.contains(e.target)) { // Si el clic está fuera del contenido del modal
+            modal.classList.add('hidden');
+        }
+    });
+});
+
+    </script>
