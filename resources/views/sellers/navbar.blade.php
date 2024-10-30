acd969fbd67c1b2614d375782fa30c911a9f8215
Alejandro Robles Bolaños <alejandrorb0803@gmail.com>
2024-10-30 13:22:37 -0600

    update button of search

--- resources/views/sellers/navbar.blade.php
+++ resources/views/sellers/navbar.blade.php
@@ -10,8 +10,10 @@
             <!-- Search -->
             <div class="relative">
                 <form action="">
-                     <input type="text" placeholder="¿Cuál servicio buscas hoy?" class="w-[600px] placeholder:text-[18px] placeholder:font-light bg-transparent border-blue border-[0.5px] border-opacity-50 rounded-[16px] p-[10px] outline-none text-[18px] text-black font-light">
-                    <button class="bg-blue absolute rounded-[8px] p-[8px] right-[10px] top-[50%] translate-y-[-50%]"><img class="w-[18px]" src="{{ asset('icons/search.svg') }}" alt=""></button>
+                <div class="relative">
+                        <input type="text" placeholder="¿Cuál servicio buscas hoy?" class="w-[600px] placeholder:text-[18px] placeholder:font-light bg-transparent border-blue border-[0.5px] border-opacity-50 rounded-[16px] p-[10px] outline-none text-[18px] text-black font-light">
+                        <button class="bg-blue absolute rounded-[8px] p-[8px] right-[10px] top-[50%] translate-y-[-50%] items-center"><img class="w-[18px]" src="{{ asset('icons/search.svg') }}" alt=""></button>
+                    </div>
                 </form>
             </div>
 
